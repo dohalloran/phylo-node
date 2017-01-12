b8f257950a485039d2412edaa0e55313bbb61b2d
ohalloranlab <dohalloran@users.noreply.github.com>
2017-01-12 12:48:18 -0500

    Update index.js

--- Tools/Run/Kalign/index.js
+++ Tools/Run/Kalign/index.js
@@ -4,5 +4,5 @@ var base = require('../../../Wrapper_Core/base-wrap');
 var kalign = require('./kalign.js');
 //get Path for output
 var outFile = './Output/kalign_Result.aln';
-//run Clustal program
+//run kalign program
 base.call_(process.argv[2], outFile, process.argv, kalign.run_);
